a486f4930a5a2e5deb1d76dbdc9355a5c70bf5ec
Mothilal-MJ <lalihtom@gmail.com>
2022-11-26 12:49:55 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+// import { User } from './domain/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vicky';
+
+  userForm: FormGroup = new FormGroup({
+    firstname: new FormControl(""),
+    surname: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+    dateofbirth: new FormControl(""),
+    gender: new FormControl(""),
+  })
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder){}
+  ngOnInit(){
+    this.userForm = this.formBuilder.group({
+    firstname: ["",Validators.required],
+    surname: ["",Validators.required],
+    email: ["",[Validators.required,Validators.email]],
+    password: ["",[Validators.required, Validators.minLength(8),Validators.maxLength(25)]],
+    dateofbirth: ["",Validators.required],
+    gender: ["",Validators.required],
+    })
+  }
+  get f():{[key:string]: AbstractControl}{
+    return this.userForm.controls;
+  }
+  yearRange(start: number, end: number): number[] {
+    return Array(Math.ceil((end - start) / 1)).fill(start).map((x, y) => x + y * 1)
+  }
+  // user = new User();
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.userForm.invalid){
+      return;
+    }
+    // this.userForm.get('gender').value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// import { NgxRangeModule } from 'ngx-range';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    // NgxRangeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
